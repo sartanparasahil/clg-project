6ce6a2af1f8f8cdd7af153766f391fb4fdbacaca
sartanpara-sahil <sartanparasahilc123@gmail.com>
2023-08-07 11:40:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let navlinks = document.getElementById("nav-item");
+let menubar = document.getElementById("menu-btn");
+let cancel = document.getElementById("cancel-btn");
+
+const openmenu = () => {
+    navlinks.classList.add("active");
+}
+const closemenu = () => {
+    navlinks.classList.remove("active");
+}
+
+menubar.addEventListener("click", openmenu);
+cancel.addEventListener("click", closemenu);
