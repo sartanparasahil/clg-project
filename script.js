d1b77fc462c19c2182f08ffdc40e299c705a06cc
sartanpara-sahil <sartanparasahilc123@gmail.com>
2023-08-07 23:18:55 +0530

    7-8-2023

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let navlinks = document.getElementById("nav-item");
 let menubar = document.getElementById("menu-btn");
 let cancel = document.getElementById("cancel-btn");
+let navlogo = document.getElementById("nav-link-logo");
 
 const openmenu = () => {
     navlinks.classList.add("active");
+    navlogo.classList.add("active");
 }
 const closemenu = () => {
     navlinks.classList.remove("active");
+    navlogo.classList.add("active");
 }
 
 menubar.addEventListener("click", openmenu);
