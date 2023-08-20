0da847f244d589c32023450ef2700aa6ab90fe4b
sartanpara-sahil <sartanparasahilc123@gmail.com>
2023-08-20 18:51:15 +0530

    20-08-2023

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ let cancel = document.getElementById("cancel-btn");
 
 const openmenu = () => {
     navlinks.classList.add("active");
-    
+
 }
 const closemenu = () => {
     navlinks.classList.remove("active");
 
 }
-
 menubar.addEventListener("click", openmenu);
 cancel.addEventListener("click", closemenu);
+
+//sticky navbar
+const nav = document.getElementById("nav");
+window.addEventListener("scroll", () => {
+    if (scrollY > 20) {
+        nav.style.background = "rgba(0, 0, 0, 0.9)";
+        nav.style.boxShadow = " 3px 2px 5px rgb(245, 245, 244, 0.2)";
+    }
+    else {
+        nav.style.background = "none";
+        nav.style.boxShadow = "none";
+    }
+})
